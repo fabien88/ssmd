b550803b33b6d7b548c000a51ce6810e9a0fc251
Fabien Lenoir <fabien.lenoir@gmail.com>
2020-06-27 08:09:26 +0200

    UPD - 1.0.7 using npm fork

--- src/markdownParser.js
+++ src/markdownParser.js
@@ -1,8 +1,8 @@
 /**
  * Parse markdown syntax and return ssml equivalent
  */
-const Parser = require("simple-text-parser");
-const R = require('ramda');
+const Parser = require("@fabien88/simple-text-parser");
+const R = require("ramda");
 const parser = new Parser();
 
 // Handle functions like extensions etc...
@@ -11,7 +11,7 @@ const addParseFunc = (name, callback) => {
   parser.addRule(regex, (tag, text) => {
     const [, , param] = new RegExp(regex, "gi").exec(tag);
     return {
-      type: s => callback(s, text, param)
+      type: (s) => callback(s, text, param),
     };
   });
 };
@@ -22,10 +22,11 @@ const rate = ["", "x-slow", "slow", "medium", "fast", "x-fast"];
 const pitch = ["", "x-low", "low", "medium", "high", "x-high"];
 addParseFunc("vrp", (s, text, param) => {
   const [vIdx, rIdx, pIdx] = param;
-  return s.prosody({
+  return s.prosody(
+    {
       volume: volumes[vIdx],
       rate: rate[rIdx],
-      pitch: pitch[pIdx]
+      pitch: pitch[pIdx],
     },
     text
   );
@@ -34,13 +35,13 @@ addParseFunc("vrp", (s, text, param) => {
 addParseFunc("as", (s, text, params) =>
   s.sayAs({
     word: text,
-    interpret: params
+    interpret: params,
   })
 );
 
 const extensions = {
   whisper: (text, s) => s.whisper(text),
-  audio: (text, s) => s.audio(text)
+  audio: (text, s) => s.audio(text),
 };
 addParseFunc("ext", (s, text, extension) => extensions[extension](text, s));
 
@@ -50,109 +51,162 @@ parser.addRule(audioRegex, (tag, text) => {
   const [, desc, param, alt] = new RegExp(audioRegex).exec(tag);
   return {
     type: "text",
-    text: `<audio src="${param}">${desc && `<desc>${desc}</desc>`}${alt}</audio>`
+    text: `<audio src="${param}">${
+      desc && `<desc>${desc}</desc>`
+    }${alt}</audio>`,
   };
 });
 
-
 // Emphasis
 parser.addRule(/\*\*(.+?)\*\*/gi, (tag, text) => ({
-  type: s => s.emphasis("strong", text)
+  type: (s) => s.emphasis("strong", text),
 }));
 parser.addRule(/~\*(.+?)\*~/gi, (tag, text) => ({
-  type: s => s.emphasis("reduced", text)
+  type: (s) => s.emphasis("reduced", text),
 }));
 parser.addRule(/\*(.+?)\*/gi, (tag, text) => ({
-  type: s => s.emphasis("moderate", text),
-  text
+  type: (s) => s.emphasis("moderate", text),
+  text,
 }));
 
 // Volume
 parser.addRule(/~(.+?)~/gi, (tag, text) => ({
-  type: s => s.prosody({
-    volume: "silent"
-  }, text)
+  type: (s) =>
+    s.prosody(
+      {
+        volume: "silent",
+      },
+      text
+    ),
 }));
 parser.addRule(/--(.+?)--/gi, (tag, text) => ({
-  type: s => s.prosody({
-    volume: "x-soft"
-  }, text)
+  type: (s) =>
+    s.prosody(
+      {
+        volume: "x-soft",
+      },
+      text
+    ),
 }));
 parser.addRule(/-(.+?)-/gi, (tag, text) => ({
-  type: s => s.prosody({
-    volume: "soft"
-  }, text)
+  type: (s) =>
+    s.prosody(
+      {
+        volume: "soft",
+      },
+      text
+    ),
 }));
 parser.addRule(/\+\+(.+?)\+\+/gi, (tag, text) => ({
-  type: s => s.prosody({
-    volume: "x-loud"
-  }, text)
+  type: (s) =>
+    s.prosody(
+      {
+        volume: "x-loud",
+      },
+      text
+    ),
 }));
 parser.addRule(/\+(.+?)\+/gi, (tag, text) => ({
-  type: s => s.prosody({
-    volume: "loud"
-  }, text)
+  type: (s) =>
+    s.prosody(
+      {
+        volume: "loud",
+      },
+      text
+    ),
 }));
 
 // Rate
 parser.addRule(/<<(.+?)<</gi, (tag, text) => ({
-  type: s => s.prosody({
-    rate: "x-slow"
-  }, text)
+  type: (s) =>
+    s.prosody(
+      {
+        rate: "x-slow",
+      },
+      text
+    ),
 }));
 parser.addRule(/<(.+?)</gi, (tag, text) => ({
-  type: s => s.prosody({
-    rate: "slow"
-  }, text)
+  type: (s) =>
+    s.prosody(
+      {
+        rate: "slow",
+      },
+      text
+    ),
 }));
 parser.addRule(/>>(.+?)>>/gi, (tag, text) => ({
-  type: s => s.prosody({
-    rate: "x-fast"
-  }, text)
+  type: (s) =>
+    s.prosody(
+      {
+        rate: "x-fast",
+      },
+      text
+    ),
 }));
 parser.addRule(/>(.+?)>/gi, (tag, text) => ({
-  type: s => s.prosody({
-    rate: "fast"
-  }, text)
+  type: (s) =>
+    s.prosody(
+      {
+        rate: "fast",
+      },
+      text
+    ),
 }));
 
 // Pitch
 parser.addRule(/__(.+?)__/gi, (tag, text) => ({
-  type: s => s.prosody({
-    pitch: "x-low"
-  }, text)
+  type: (s) =>
+    s.prosody(
+      {
+        pitch: "x-low",
+      },
+      text
+    ),
 }));
 parser.addRule(/_(.+?)_/gi, (tag, text) => ({
-  type: s => s.prosody({
-    pitch: "low"
-  }, text)
+  type: (s) =>
+    s.prosody(
+      {
+        pitch: "low",
+      },
+      text
+    ),
 }));
 parser.addRule(/\^\^(.+?)\^\^/gi, (tag, text) => ({
-  type: s => s.prosody({
-    pitch: "x-high"
-  }, text)
+  type: (s) =>
+    s.prosody(
+      {
+        pitch: "x-high",
+      },
+      text
+    ),
 }));
 parser.addRule(/\^(.+?)\^/gi, (tag, text) => ({
-  type: s => s.prosody({
-    pitch: "high"
-  }, text)
+  type: (s) =>
+    s.prosody(
+      {
+        pitch: "high",
+      },
+      text
+    ),
 }));
 
 // Pause and break
 const pauseRegex = /\.\.\.(\S+)(s|ms)/gi;
-parser.addRule(new RegExp(pauseRegex), tag => {
+parser.addRule(new RegExp(pauseRegex), (tag) => {
   const [, time, unit] = new RegExp(pauseRegex).exec(tag);
   return {
-    type: s => s.pause(time + unit)
+    type: (s) => s.pause(time + unit),
   };
 });
 
 parser.addRule(/(\.\.\.)/gi, () => ({
-  type: s => s.pause("1000ms")
+  type: (s) => s.pause("1000ms"),
 }));
 
 function required(field) {
-  throw new Error(field + ' is required in headingLevel config !');
+  throw new Error(field + " is required in headingLevel config !");
 }
 
 // Heading, by default, use emphasis and break
@@ -161,31 +215,22 @@ parser.addRule(/^\s*(#+)\s*(.+)/gi, (tag, hashes, text) => {
   const headingLevel = hashes.length;
 
   // Apply tag on current speech object
-  const execTag = (s, {
-    tag = required('tag'),
-    value
-  }) => {
+  const execTag = (s, { tag = required("tag"), value }) => {
     value = R.defaultTo([], value);
     if (!R.is(Array, value)) {
       value = [value];
     }
     return s[tag](...[...value, text]);
-  }
+  };
 
   return {
-    type: (s, {
-      headingLevels
-    }) => {
+    type: (s, { headingLevels }) => {
       if (!headingLevels[headingLevel]) {
         return s.say(tag);
       }
-      return R.reduce(execTag, s, headingLevels[headingLevel])
-    }
-
-  }
-
+      return R.reduce(execTag, s, headingLevels[headingLevel]);
+    },
+  };
 });
-
-
 
 module.exports = parser;
